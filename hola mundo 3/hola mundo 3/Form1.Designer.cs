540373f64c3640f74383dcf64a7d36eff5c2bc16
EDWING OVALLES <edwingovalles@hotmail.com>
2017-07-08 12:49:34 -0400

    hola

---
+++ hola mundo 3/hola mundo 3/Form1.Designer.cs
@@ -0,0 +1,61 @@
+﻿namespace hola_mundo_3
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblholamundo = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // lblholamundo
+            // 
+            this.lblholamundo.AutoSize = true;
+            this.lblholamundo.Location = new System.Drawing.Point(85, 108);
+            this.lblholamundo.Name = "lblholamundo";
+            this.lblholamundo.Size = new System.Drawing.Size(65, 13);
+            this.lblholamundo.TabIndex = 0;
+            this.lblholamundo.Text = "holamundo3";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 261);
+            this.Controls.Add(this.lblholamundo);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblholamundo;
+    }
+}
+
